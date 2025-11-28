f9cc5e3506be8dbadb947949a8b5d8ece0831605
22Wilber22 <wilbergrande308@gmail.com>
2025-11-28 16:39:18 -0600

    feat: allow PATCH method in CORS configuration

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(cors({
     'http://localhost:3000' // para desarrollo
   ],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
